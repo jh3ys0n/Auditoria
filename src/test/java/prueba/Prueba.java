4d3761da2266df2e1fb99b2db06e0bcd35a85eb7
jh3ys0n <gazelaldena@gmail.com>
2022-10-08 14:21:59 -0400

    mejorando codigo

--- src/test/java/prueba/Prueba.java
+++ src/test/java/prueba/Prueba.java
@@ -10,29 +10,39 @@ public class Prueba {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("hola");
 		
-		// Optional. If not specified, WebDriver searches the PATH for chromedriver.       
-		
 		System.setProperty("webdriver.chrome.driver", "C:/Users/2022/Documents/Scanned Documents/chromedriver.exe");           
 		
 		WebDriver driver = new ChromeDriver(); 
 
 		driver.get("http://the-internet.herokuapp.com/login");     
 		
-		driver.findElement(By.name("username")).sendKeys("jeyson bartolome");
-		driver.findElement(By.id("password")).sendKeys("Urrelo");
+		driver.findElement(By.name("username")).sendKeys("tomsmith");
+		
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
 		
 		driver.findElement(By.cssSelector("button")).click();
 		
-	//	driver.findElement(By.id("username")).sendKeys("tomsmith");       
-	//	driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");       
-		  //example  
-	//	driver.findElement(By.cssSelector("button")).click();   
 		
-	System.out.println(driver.findElement(By.tagName("h2")).getText());	
-		//   icon-lock  assertTrue("success message not present",             
-		  //      driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
-		  //driver.quit();  
+		if(driver.findElement(By.tagName("h2")).getText().compareTo("Secure Area")==0) {
+			System.out.println("LOS TITULOS SON IGUALES");
+		}
+		else {
+			System.out.println("NO SON IGUALES");
+		}
 
 	}
 
 }
+
+
+
+
+//driver.findElement(By.id("username")).sendKeys("tomsmith");       
+//	driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");       
+	  //example  
+//	driver.findElement(By.cssSelector("button")).click();   
+	
+//System.out.println(driver.findElement(By.tagName("h2")).getText());	
+	//   icon-lock  assertTrue("success message not present",             
+	  //      driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+	  //driver.quit();  
